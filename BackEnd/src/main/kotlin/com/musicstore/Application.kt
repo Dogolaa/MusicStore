8e9c600dfff6e52650899a773331ef7be8813073
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-11-18 13:12:28 -0300

    fix: habilitando CORS

--- BackEnd/src/main/kotlin/com/musicstore/Application.kt
+++ BackEnd/src/main/kotlin/com/musicstore/Application.kt
@@ -6,13 +6,32 @@ import com.musicstore.plugins.configureSerialization
 import com.musicstore.repositories.brand.PostgresBrandRepository
 import com.musicstore.repositories.product.PostgresProductRepository
 import com.musicstore.repositories.user.PostgresUserRepository
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.CORS
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+
+        // Cabeçalhos permitidos
+        allowHeader(HttpHeaders.ContentType)
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader("api-key")
+
+        // Permitir credenciais (se necessário)
+        anyHost()
+    }
+
     // Instâncias da pasta de plugins
     configureSecurity()
     configureSerialization(
