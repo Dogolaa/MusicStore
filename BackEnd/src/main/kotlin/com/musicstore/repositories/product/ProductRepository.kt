d37feee59252f1ef6173084a755c97e9e6e44576
lucas ferrari <lucas.carrijoferrari@gmail.com>
2024-11-18 16:42:09 -0300

    fix: lógica de ordenação refatorada e corrigida

--- BackEnd/src/main/kotlin/com/musicstore/repositories/product/ProductRepository.kt
+++ BackEnd/src/main/kotlin/com/musicstore/repositories/product/ProductRepository.kt
@@ -1,10 +1,16 @@
 package com.musicstore.repositories.product
 
 import com.musicstore.model.Product
+import com.musicstore.model.request.ProductPaginatedResponse
 import com.musicstore.model.request.UpdateProduct
 
 interface ProductRepository {
-    suspend fun allProducts(ascending: Boolean = true, offset: Int = 0, limit: Int = 10): List<Product>
+    suspend fun allProducts(
+        ascending: Boolean = true,
+        page: Int = 1,
+        pageSize: Int = 10
+    ): ProductPaginatedResponse<Product>
+
     suspend fun addProduct(product: Product)
     suspend fun productById(id: Int): Product?
     suspend fun updateProductById(id: Int, product: UpdateProduct)
