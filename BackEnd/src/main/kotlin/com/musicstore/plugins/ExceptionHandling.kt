9d142b149c14462dbb2edff715fb98e197e37832
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-11-28 23:54:03 -0300

    feat: exception handling funcionando e corretamente implementado

--- BackEnd/src/main/kotlin/com/musicstore/plugins/ExceptionHandling.kt
+++ BackEnd/src/main/kotlin/com/musicstore/plugins/ExceptionHandling.kt
@@ -1,31 +1,41 @@
 package com.musicstore.plugins
 
 import com.musicstore.model.request.ErrorResponse
-import io.ktor.http.*
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
-import io.ktor.server.plugins.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 
 fun Application.configureExceptionHandling() {
     install(StatusPages) {
-        exception<NotFoundException> { call, cause ->
+        exception<com.musicstore.exceptions.NotFoundException> { call, cause ->
             call.respond(
                 HttpStatusCode.NotFound,
                 ErrorResponse(
                     HttpStatusCode.NotFound.value,
-                    cause.message?: "Resource not found",
-                    null
+                    cause.message ?: "Bad Request",
+                    cause.details
                 )
             )
         }
 
-        exception<NotFoundException> { call, cause ->
+        exception<com.musicstore.exceptions.MissingHeaderException> { call, cause ->
             call.respond(
                 HttpStatusCode.BadRequest,
                 ErrorResponse(
                     HttpStatusCode.BadRequest.value,
-                    cause.message?: "Bad Request",
+                    cause.message ?: "Bad Request",
+                    cause.details
+                )
+            )
+        }
+
+        exception<Throwable> { call, cause ->
+            call.respond(
+                HttpStatusCode.InternalServerError,
+                ErrorResponse(
+                    HttpStatusCode.InternalServerError.value,
+                    cause.message ?: "Internal Server Error",
                     null
                 )
             )
