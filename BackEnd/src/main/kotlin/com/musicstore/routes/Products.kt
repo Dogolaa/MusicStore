953e3ee4c05ccf47c081aad9f398d21d7db298c1
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-12-05 10:47:28 -0300

    fix: revertendo autenticação

--- BackEnd/src/main/kotlin/com/musicstore/routes/Products.kt
+++ BackEnd/src/main/kotlin/com/musicstore/routes/Products.kt
@@ -2,7 +2,6 @@ package com.musicstore.routes
 
 import com.musicstore.model.Product
 import com.musicstore.model.request.UpdateProduct
-import com.musicstore.plugins.RoleValidation
 import com.musicstore.repositories.product.ProductRepository
 import io.ktor.http.*
 import io.ktor.http.content.*
@@ -16,112 +15,109 @@ import kotlinx.io.readByteArray
 import java.io.File
 
 fun Route.productRoute(productRepository: ProductRepository) {
-    authenticate("auth-jwt") {
-        route("/api/products") {
-            install(RoleValidation) {
-                roles = listOf("SELLER")
-            }
 
-            get {
-                val principal = call.principal<JWTPrincipal>()
-                println("JWT Principal: $principal")
-
-                val ascending = call.request.queryParameters["asc"]?.toBoolean() != false
-                val page = call.request.queryParameters["page"]?.toIntOrNull()?.coerceAtLeast(1) ?: 1
-                val nameProduct = call.request.queryParameters["nameProduct"]
-                val shortDesc = call.request.queryParameters["shortDesc"]
-                val fullDesc = call.request.queryParameters["fullDesc"]
-                val brandId = call.request.queryParameters["brand"]?.toIntOrNull()
-                val categoryId = call.request.queryParameters["category"]?.toIntOrNull()
-
-                val paginatedProducts = productRepository.allProducts(
-                    ascending = ascending,
-                    page = page,
-                    nameProduct = nameProduct,
-                    shortDesc = shortDesc,
-                    fullDesc = fullDesc,
-                    brandId = brandId,
-                    categoryId = categoryId
-                )
-
-                call.respond(paginatedProducts)
-            }
+    route("/api/products") {
 
 
-            get("/{id}") {
-                val id = call.parameters["id"] ?: return@get call.respond(HttpStatusCode.BadRequest)
-                val product =
-                    productRepository.productById(id.toInt()) ?: return@get call.respond(HttpStatusCode.NotFound)
-                call.respond(product)
-            }
+        get {
+            val principal = call.principal<JWTPrincipal>()
+            println("JWT Principal: $principal")
 
-            post {
-                try {
-                    var productJson = ""
-                    var fileName = ""
+            val ascending = call.request.queryParameters["asc"]?.toBoolean() != false
+            val page = call.request.queryParameters["page"]?.toIntOrNull()?.coerceAtLeast(1) ?: 1
+            val nameProduct = call.request.queryParameters["nameProduct"]
+            val shortDesc = call.request.queryParameters["shortDesc"]
+            val fullDesc = call.request.queryParameters["fullDesc"]
+            val brandId = call.request.queryParameters["brand"]?.toIntOrNull()
+            val categoryId = call.request.queryParameters["category"]?.toIntOrNull()
 
-                    val multipartData = call.receiveMultipart()
-                    multipartData.forEachPart { part ->
-                        when (part) {
-                            is PartData.FormItem -> {
-                                if (part.name == "product") {
-                                    productJson = part.value
-                                }
+            val paginatedProducts = productRepository.allProducts(
+                ascending = ascending,
+                page = page,
+                nameProduct = nameProduct,
+                shortDesc = shortDesc,
+                fullDesc = fullDesc,
+                brandId = brandId,
+                categoryId = categoryId
+            )
+
+            call.respond(paginatedProducts)
+        }
+
+
+        get("/{id}") {
+            val id = call.parameters["id"] ?: return@get call.respond(HttpStatusCode.BadRequest)
+            val product =
+                productRepository.productById(id.toInt()) ?: return@get call.respond(HttpStatusCode.NotFound)
+            call.respond(product)
+        }
+
+        post {
+            try {
+                var productJson = ""
+                var fileName = ""
+
+                val multipartData = call.receiveMultipart()
+                multipartData.forEachPart { part ->
+                    when (part) {
+                        is PartData.FormItem -> {
+                            if (part.name == "product") {
+                                productJson = part.value
                             }
-
-                            is PartData.FileItem -> {
-                                if (part.name == "image") {
-                                    val originalFileName = part.originalFileName ?: "unknown.jpg"
-                                    val fileExtension = originalFileName.substringAfterLast(".", "jpg")
-                                    fileName =
-                                        "${System.currentTimeMillis()}-${java.util.UUID.randomUUID()}.$fileExtension"
-
-                                    val fileBytes = part.provider().readRemaining().readByteArray()
-                                    File("images/products/$fileName").writeBytes(fileBytes)
-                                }
-                            }
-
-                            else -> {}
                         }
-                        part.dispose()
+
+                        is PartData.FileItem -> {
+                            if (part.name == "image") {
+                                val originalFileName = part.originalFileName ?: "unknown.jpg"
+                                val fileExtension = originalFileName.substringAfterLast(".", "jpg")
+                                fileName =
+                                    "${System.currentTimeMillis()}-${java.util.UUID.randomUUID()}.$fileExtension"
+
+                                val fileBytes = part.provider().readRemaining().readByteArray()
+                                File("images/products/$fileName").writeBytes(fileBytes)
+                            }
+                        }
+
+                        else -> {}
                     }
-
-                    val product = kotlinx.serialization.json.Json.decodeFromString(Product.serializer(), productJson)
-                    val productWithImage = product.copy(product_main_photo = fileName)
-
-                    productRepository.addProduct(productWithImage)
-                    call.respond(HttpStatusCode.Created, "Produto adicionado com sucesso!")
-                } catch (ex: Exception) {
-                    call.respond(
-                        HttpStatusCode.InternalServerError,
-                        "Erro inesperado: ${ex.message ?: "Entre em contato com o suporte."}"
-                    )
+                    part.dispose()
                 }
+
+                val product = kotlinx.serialization.json.Json.decodeFromString(Product.serializer(), productJson)
+                val productWithImage = product.copy(product_main_photo = fileName)
+
+                productRepository.addProduct(productWithImage)
+                call.respond(HttpStatusCode.Created, "Produto adicionado com sucesso!")
+            } catch (ex: Exception) {
+                call.respond(
+                    HttpStatusCode.InternalServerError,
+                    "Erro inesperado: ${ex.message ?: "Entre em contato com o suporte."}"
+                )
+            }
+        }
+
+        put("/{id}") {
+            val id = call.parameters["id"]
+            if (id == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@put
             }
 
-            put("/{id}") {
-                val id = call.parameters["id"]
-                if (id == null) {
-                    call.respond(HttpStatusCode.BadRequest)
-                    return@put
-                }
+            val product = call.receive<UpdateProduct>()
+            productRepository.updateProductById(id.toInt(), product)
+            call.respond(HttpStatusCode.OK)
+        }
 
-                val product = call.receive<UpdateProduct>()
-                productRepository.updateProductById(id.toInt(), product)
+        delete("/{id}") {
+            val id = call.parameters["id"]
+            if (id == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@delete
+            }
+            if (productRepository.removeProductById(id.toInt())) {
                 call.respond(HttpStatusCode.OK)
-            }
-
-            delete("/{id}") {
-                val id = call.parameters["id"]
-                if (id == null) {
-                    call.respond(HttpStatusCode.BadRequest)
-                    return@delete
-                }
-                if (productRepository.removeProductById(id.toInt())) {
-                    call.respond(HttpStatusCode.OK)
-                } else {
-                    call.respond(HttpStatusCode.NotFound)
-                }
+            } else {
+                call.respond(HttpStatusCode.NotFound)
             }
         }
     }
